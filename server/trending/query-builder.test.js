9e5db95525f7e8a7e0c9b7d830d55e1084e8f82e
Rich Hagarty <rich.hagarty@ibm.com>
2017-06-20 21:40:33 -0700

    fix tests

--- server/trending/query-builder.test.js
+++ server/trending/query-builder.test.js
@@ -12,9 +12,6 @@ describe('Query builder returns params for discovery service', () => {
       environment_id: 'environment',
       collection_id: 'collection',
       return: 'enrichedTitle.entities.text',
-      aggregations: [
-        'term(enrichedTitle.entities.text,count:20).top_hits(1)'
-      ],
       filter: `blekko.chrondate>${moment().subtract(24,'h').unix()},blekko.hostrank>300`
     });
   });
@@ -26,9 +23,6 @@ describe('Query builder returns params for discovery service', () => {
       environment_id: 'environment',
       collection_id: 'collection',
       return: 'enrichedTitle.entities.text',
-      aggregations: [
-        'term(enrichedTitle.entities.text,count:20).top_hits(1)'
-      ],
       filter: `taxonomy.label:"test",blekko.chrondate>${moment().subtract(24,'h').unix()},blekko.hostrank>300`
     });
   });
